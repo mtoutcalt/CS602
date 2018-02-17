30a8be1ec4d898af3f6927eddc6510c5809c731c
mtoutcalt <mtoutcalt@gmail.com>
2018-02-16 20:49:36 -0500

    order a game

--- CS602_TermProject_Outcalt/controllers/game/saveAfterPurchase.js
+++ CS602_TermProject_Outcalt/controllers/game/saveAfterPurchase.js
@@ -3,7 +3,7 @@ const Game = DB.getGameModel();
 
 /////SAVE ORDER by decrementing the quantity and checking for 0
 
-module.exports = function saveGame(req, res, next) {
+module.exports = function saveAfterPurchase(req, res, next) {
   let id = req.params.id;
 
   Game.findById(id, (err, game) => {
@@ -14,17 +14,30 @@ module.exports = function saveGame(req, res, next) {
       return res.render('404');
     }
 
+
     game.name = req.body.gameName; //get data from body of post
     game.description = req.body.gameDescription;
     game.price = req.body.gamePrice;
-    game.quantity = req.body.gameQuantity;
+
+    let currentQuantity = game.quantity;
+    console.log("CURRENT QUANT: " + currentQuantity);
+
+    let copiesLeft = currentQuantity - req.body.gameQuantity;
+    console.log("COPIES LEFT: " + copiesLeft);
+
+    if (copiesLeft < 0) {
+      console.log("NO COPIES LEFT");
+      return res.render('outOfStockView', {title: "Out of Stock", data: {copiesLeft: currentQuantity}});
+    }
+
+    game.quantity = copiesLeft;
 
 
     game.save( (err) => {
       if (err) {
         console.log("Error updating : %s ", err);
       }
-      res.redirect('/games');
+      res.redirect('/store');
     });
 
   });
